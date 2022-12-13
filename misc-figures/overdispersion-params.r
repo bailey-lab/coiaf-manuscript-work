85f667f5eb2094e314440fa283ad42b81948b662
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-12-13 14:50:56 -0500

    Improve simulation parameter plots

--- misc-figures/overdispersion-params.r
+++ misc-figures/overdispersion-params.r
@@ -1,3 +1,7 @@
+library(coiaf)
+library(tidyverse)
+library(patchwork)
+
 # Plot for impact of changing alpha parameter ----------------------------------
 LaplacesDemon::ddirichlet(c(.1, .3, .6), rep(1, 3))
 LaplacesDemon::rdirichlet(10, rep(1, 3))
@@ -11,7 +15,6 @@ pars$mean <- 0
 pars$sd <- 0
 
 # Generate data
-library(coiaf)
 for (i in seq_along(pars$coi)) {
   m <- replicate(1000, max(coiaf:::rdirichlet(rep(pars$alpha[i], pars$coi[i]))))
   pars$mean[i] <- mean(m)
@@ -19,8 +22,7 @@ for (i in seq_along(pars$coi)) {
 }
 
 # Plot
-library(tidyverse)
-ggplot(pars, aes(alpha, mean, color = as.factor(coi))) +
+alpha_plot <- ggplot(pars, aes(alpha, mean, color = as.factor(coi))) +
   geom_hline(
     data = data.frame(coi = cois, y = 1 / cois),
     aes(yintercept = y, color = as.factor(coi)),
@@ -36,19 +38,9 @@ ggplot(pars, aes(alpha, mean, color = as.factor(coi))) +
   # xlab("Alpha (Parameter controlling overdispersion \nof parasite densities)") +
   xlab("Alpha") +
   # ggtitle("Dashed lines show maximum parasite density if \nall strains had the same parasite density, i.e. 1/COI") +
-  scale_color_discrete(name = "COI") +
+  scale_colour_viridis_d(name = "COI", option = "H") +
   theme_coiaf() +
   theme(plot.title = element_text(size = 10))
-
-# Save
-ggsave(
-  filename = here::here("misc-figures", "alpha-param.png"),
-  device = "png",
-  width = 2000,
-  height = 1750,
-  units = "px",
-  dpi = "print"
-)
 
 # Plot for impact of changing overdispersion parameter -------------------------
 # Set params and get data
@@ -69,22 +61,27 @@ for (i in seq_along(overd)) {
 }
 
 # Plot
-do.call(rbind, res) %>%
+over_plot <- do.call(rbind, res) %>%
   ggplot(aes(counts, prob, color = as.factor(overdispersion))) +
   geom_vline(xintercept = 50, linetype = "dashed") +
   geom_line() +
   ylab("Probability Density") +
   xlab("Reference Reads") +
-  scale_color_discrete(name = "Overdispersion\nin Sequencing") +
+  scale_colour_viridis_d(name = "Overdispersion\nin Sequencing") +
   theme_coiaf() +
-  ggtitle("Sequence Coverage = 100, Expected Reference Reads = 50") +
+  ggtitle("Sequence Coverage = 100,\nExpected Reference Reads = 50") +
   theme(plot.title = element_text(size = 10))
+
+# Combine plots ----------------------------------------------------------------
+alpha_plot + over_plot +
+  plot_annotation(tag_levels = "A") &
+  theme(plot.tag = element_text(face = 'bold'))
 
 # Save
 ggsave(
-  filename = here::here("misc-figures", "over-param.png"),
+  filename = here::here("misc-figures", "sim-param.png"),
   device = "png",
-  width = 2000,
+  width = 3000,
   height = 1750,
   units = "px",
   dpi = "print"
